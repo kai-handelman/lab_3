4e53c19ab5ef40fc5ec24efba32724ff54f7bc7c
kai-handelman <kai.handelman@gmail.com>
2019-09-20 13:27:05 -0600

    Blah creating file practice.sh also storing txt

---
+++ practice.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Author : YOur name
+#date: sept 20
+#the script
+echo "Enter a Num: "
+read numOne
+echo "Enter another num: "
+read numTwo
+sum=$(($numOne + $numTwo))
+let prod=$numOne*$numTwo
+echo "The sum is $sum"
+echo "The prod is $prod"
+echo "File namde $0"
+echo "Command line 1 $1"
+grep $1 $2
